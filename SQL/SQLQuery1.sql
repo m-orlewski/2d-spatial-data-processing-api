91f046d5ffba7fff69ad63e50f3bf7bbf6134cb3
morlewski <morlewski@protonmail.com>
2022-06-20 19:43:00 +0200

    update sql script

--- SQL/SQLQuery1.sql
+++ SQL/SQLQuery1.sql
@@ -1,7 +1,13 @@
-DROP TABLE dbo.Points;
+DROP TABLE IF EXISTS dbo.Points;
+DROP TABLE IF EXISTS dbo.Circles;
+DROP TABLE IF EXISTS dbo.Triangles;
+DROP TABLE IF EXISTS dbo.Quadrangles;
 GO
 
 CREATE TABLE Points(point Point);
+CREATE TABLE Circles(circle Circle);
+CREATE TABLE Triangles(triangle Triangle);
+CREATE TABLE Quadrangles(quadrangle Quadrangle);
 GO
 
 INSERT INTO dbo.Points (point) VALUES (CONVERT(Point, '(0,0)'));
@@ -10,34 +16,29 @@ INSERT INTO dbo.Points (point) VALUES (CONVERT(Point, '(2,2)'));
 INSERT INTO dbo.Points (point) VALUES (CONVERT(Point, '(3,3)'));
 GO
 
-SELECT point.ToString() AS "Punkt", point.X as "X", point.Y as "Y" FROM dbo.Points;
-GO
-
-DROP TABLE dbo.Circles;
-GO
-
-CREATE TABLE Circles(circle Circle);
-GO
-
 INSERT INTO dbo.Circles (circle) VALUES (CONVERT(Circle, 'c=(1,1) r=1'));
 INSERT INTO dbo.Circles (circle) VALUES (CONVERT(Circle, 'c=(2,2) r=2'));
 INSERT INTO dbo.Circles (circle) VALUES (CONVERT(Circle, 'c=(3,3) r=3'));
 INSERT INTO dbo.Circles (circle) VALUES (CONVERT(Circle, 'c=(4,4) r=4'));
 GO
 
-SELECT circle.ToString() AS "Okr¹g" FROM dbo.Circles;
-GO
-
-DROP TABLE dbo.Triangles;
-GO
-
-CREATE TABLE Triangles(triangle Triangle);
-GO
-
 INSERT INTO dbo.Triangles (triangle) VALUES (CONVERT(Triangle, '(0,0),(1,1),(2,2)'));
 INSERT INTO dbo.Triangles (triangle) VALUES (CONVERT(Triangle, '(3,3),(4,4),(2,2)'));
+GO
+
+INSERT INTO dbo.Quadrangles (quadrangle) VALUES (CONVERT(Quadrangle, '(0,0),(1,1),(2,2),(3,3)'));
+INSERT INTO dbo.Quadrangles (quadrangle) VALUES (CONVERT(Quadrangle, '(0,0),(1,1),(2,2),(4,4)'));
+GO
+
+
+SELECT point.ToString() AS "Punkt", point.X as "X", point.Y as "Y" FROM dbo.Points;
+GO
+
+SELECT circle.ToString() AS "Okr¹g" FROM dbo.Circles;
 GO
 
 SELECT triangle.ToString() AS "Trójk¹t" FROM dbo.Triangles;
 GO
 
+SELECT quadrangle.ToString() AS "Czworok¹t" FROM dbo.Quadrangles;
+GO
