042f2b573cba7a94ddbc1d5ee49448d8d5a943a1
morlewski <morlewski@protonmail.com>
2022-06-21 19:05:34 +0200

    add invalid inserts

--- SQL/SQLQuery1.sql
+++ SQL/SQLQuery1.sql
@@ -7,10 +7,10 @@ DROP TABLE IF EXISTS dbo.Triangles;
 DROP TABLE IF EXISTS dbo.Quadrangles;
 GO
 
-CREATE TABLE Points(point Point);
-CREATE TABLE Circles(circle Circle);
-CREATE TABLE Triangles(triangle Triangle);
-CREATE TABLE Quadrangles(quadrangle Quadrangle);
+CREATE TABLE Points(id int IDENTITY(1,1) PRIMARY KEY, point Point);
+CREATE TABLE Circles(id int IDENTITY(1,1) PRIMARY KEY, circle Circle);
+CREATE TABLE Triangles(id int IDENTITY(1,1) PRIMARY KEY, triangle Triangle);
+CREATE TABLE Quadrangles(id int IDENTITY(1,1) PRIMARY KEY, quadrangle Quadrangle);
 GO
 
 INSERT INTO dbo.Points (point) VALUES (CONVERT(Point, '(0,0)'));
@@ -22,26 +22,27 @@ GO
 INSERT INTO dbo.Circles (circle) VALUES (CONVERT(Circle, 'c=(1,1) r=1'));
 INSERT INTO dbo.Circles (circle) VALUES (CONVERT(Circle, 'c=(2,2) r=2'));
 INSERT INTO dbo.Circles (circle) VALUES (CONVERT(Circle, 'c=(3,3) r=3'));
-INSERT INTO dbo.Circles (circle) VALUES (CONVERT(Circle, 'c=(4,4) r=4'));
+INSERT INTO dbo.Circles (circle) VALUES (CONVERT(Circle, 'c=(4,4) r=-2'));
 GO
 
+INSERT INTO dbo.Triangles (triangle) VALUES (CONVERT(Triangle, '(0,0),(1,0),(1,1)'));
+INSERT INTO dbo.Triangles (triangle) VALUES (CONVERT(Triangle, '(1,0),(1,1),(0,1)'));
 INSERT INTO dbo.Triangles (triangle) VALUES (CONVERT(Triangle, '(0,0),(1,1),(2,2)'));
-INSERT INTO dbo.Triangles (triangle) VALUES (CONVERT(Triangle, '(3,3),(4,4),(2,2)'));
 GO
 
-INSERT INTO dbo.Quadrangles (quadrangle) VALUES (CONVERT(Quadrangle, '(0,0),(1,1),(2,2),(3,3)'));
+INSERT INTO dbo.Quadrangles (quadrangle) VALUES (CONVERT(Quadrangle, '(0,0),(1,0),(1,1),(0,1)'));
 INSERT INTO dbo.Quadrangles (quadrangle) VALUES (CONVERT(Quadrangle, '(0,0),(1,1),(2,2),(4,4)'));
 GO
 
 
-SELECT point.ToString() AS "Punkt", point.X as "X", point.Y as "Y" FROM dbo.Points;
+SELECT id, point.ToString() AS "Punkt", point.X as "X", point.Y as "Y" FROM dbo.Points;
 GO
 
-SELECT circle.ToString() AS "Okr¹g" FROM dbo.Circles;
+SELECT id, circle.ToString() AS "Okr¹g" FROM dbo.Circles;
 GO
 
-SELECT triangle.ToString() AS "Trójk¹t" FROM dbo.Triangles;
+SELECT id, triangle.ToString() AS "Trójk¹t" FROM dbo.Triangles;
 GO
 
-SELECT quadrangle.ToString() AS "Czworok¹t" FROM dbo.Quadrangles;
+SELECT id, quadrangle.ToString() AS "Czworok¹t" FROM dbo.Quadrangles;
 GO
